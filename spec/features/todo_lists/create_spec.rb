c3c94da9728029d6f30af97f49a9a6b87fc76558
billpsyllos <billpsyllos1@gmail.com>
2015-03-18 07:25:03 +0000

    create some tests using rspec

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'Creating todo lists' do
+  it 'redirect index page on succes' do
+    visit '/todo_lists'
+    click_link 'New Todo list'
+    expect(page).to have_content('New todo_list')
+    
+    fill_in 'Title' , with: 'My todo list'
+    fill_in 'Description' , with: 'This is what i am doing.'
+    click_button 'Create Todo list'
+    
+    expect(page).to have_content('My todo list')\
+    end
+    
+    it 'Displays an error when a todo list has not a title' do
+      expect(TodoList.count).to eq(0);
+      visit '/todo_lists'
+      click_link 'New Todo list'
+      expect(page).to have_content('New todo_list')
+    
+      fill_in 'Title' , with: ''
+      fill_in 'Description' , with: 'This is what i am doing.'
+      click_button 'Create Todo list'
+      expect(page).to have_content('error')
+      
+      visit '/todo_lists'
+      expect(TodoList.count).to eq(0);
+     
+    end
+ 
+  
+end
